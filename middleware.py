189e3140dee4630312ea0557c4b8eae558283738
Planplan <127248458+Planplan-io@users.noreply.github.com>
2023-05-02 14:14:07 +0200

    Create middleware.py

---
+++ middleware.py
@@ -0,0 +1,20 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.utils.cache import patch_vary_headers
+from django.http import HttpRequest, HttpResponse
+
+from django.utils.text import compress_string
+ 
+
+class CannonicalMiddleware(MiddlewareMixin):
+    def __init__(self, get_response):
+        super().__init__(get_response)
+        self.redirect = True
+        
+
+    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+        patch_vary_headers(response, ('Accept-Encoding',))
+
+        if response.status_code <= 400 and request.accepts('text/html'):
+                response.headers['Link'] = f'<{request.build_absolute_uri(request.path)}>; rel="canonical"'
+                
+        return response
